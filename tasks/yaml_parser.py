a9535b43fd9ffcd5f0f662d17bd66f95d96251a3
Anastasiya <atolochkova@mirantis.com>
2016-04-21 12:04:14 +0300

    Add processing_dict

--- tasks/yaml_parser.py
+++ tasks/yaml_parser.py
@@ -19,14 +19,32 @@ def count_space(lst):
     return len(lst[0])
 
 
+def processing_dict(lst):
+    yml_dict = {}
+    count = count_space(lst)
+    last_dict_key = None
+    for item in lst:
+        if item[count] != " " and item.find(":") >= 0:
+            a = item.split(":")
+            yml_dict[a[0][1:]] = a[1][1:] or []
+            if not yml_dict[a[0][1:]]:
+                last_dict_key = a[0][1:]
+        elif yml_dict:
+            yml_dict[last_dict_key].append(item)
+    res2 = {}
+    for key in yml_dict:
+        res2[key] = rec_parser(yml_dict[key])
+    return res2
+
+
 def processing_list(lst):
     yml_list = []
     count = count_space(lst)
     start_str = count * " " + "-"
     for item in lst:
         if item.startswith(start_str):
-            yml_list.append(item[count + 2: -1] or [])
-        else:
+            yml_list.append(item[count + 2:] or [])
+        elif len(yml_list):
             yml_list[len(yml_list) - 1].append(item)
     res = []
     for item in yml_list:
@@ -36,13 +54,17 @@ def processing_list(lst):
 
 def rec_parser(lst):
     if isinstance(lst, list):
-        return processing_list(lst)
+        return processing_list(lst) or processing_dict(lst)
     else:
         if lst.find(": ") >= 0:
             spl = lst.split(": ")
-            return {spl[0]: spl[1]}
+            return {spl[0]: spl[1][0:-1]}
         if lst.startswith('\'') and lst.endswith('\''):
-            return lst[1:-1]
+            return lst[1:]
+        elif lst.startswith('\'') and lst.endswith('\'\n'):
+            return lst[1:-2]
+        elif lst[0:-1].isdigit() and lst.endswith('\n'):
+            return int(lst[0:-1])
         elif lst.isdigit():
             return int(lst)
         else:
@@ -55,5 +77,5 @@ def yml_parser(file_name):
     return rec_parser(str_list)
 
 
-print yml_parser('file2.yml')
-st_parser('file2.yml')
+print yml_parser('file1.yml')
+st_parser('file1.yml')
