da3066c00cc471c9f0899b8023cc8d705e6a0b0d
Anastasiya <atolochkova@mirantis.com>
2016-04-25 13:50:53 +0300

    Add class structure_generator

--- tasks/structure_generator.py
+++ tasks/structure_generator.py
@@ -2,53 +2,54 @@ import random
 import string
 
 
-def num_creator():
-    return random.randint(0, 100)
+class structure_generator(object):
+    def __init__(self, size, depth):
+        self._size = size
+        self._depth = depth
+        self.structure = self._get_structure()
 
+    def _num_creator(self):
+        return random.randint(0, 100)
 
-def str_creator():
-    a = string.ascii_letters + string.digits
-    return ''.join([random.choice(a) for i in range(5)])
+    def _str_creator(self):
+        a = string.ascii_letters + string.digits
+        return ''.join([random.choice(a) for i in range(5)])
 
-
-def list_creator(depth):
-    result = []
-    if depth:
-        result.append(rec_generator(depth))
-        depth -= 1
-    return result or 1
-
-
-def dict_creator(depth):
-    result = {}
-    if depth:
-        result[str_creator()] = rec_generator(depth)
-        depth -= 1
-    return result or 1
-
-
-def rec_generator(depth):
-    case = random.randint(0, 3)
-    if case == 0:
-        return list_creator(depth)
-    if case == 1:
-        return dict_creator(depth)
-    if case == 2:
-        return str_creator()
-    if case == 3:
-        return num_creator()
-
-
-def structure_generator(size, depth):
-    case = random.randint(0, 1)
-    result = []
-    if case:
-        result = {}
+    def _list_creator(self, size, depth):
+        res = []
         for i in range(size):
-            result[str_creator()] = rec_generator(depth)
-    else:
-        for i in range(size):
-            result.append(rec_generator(depth))
-    return result
+            if depth:
+                res.append(self._rec_generator(size, depth))
+        return res or 1
 
-# print structure_generator(5)
+    def _dict_creator(self, size, depth):
+        res = {}
+        for i in range(size):
+            if depth:
+                res[self._str_creator()] = self._rec_generator(size, depth)
+        return res or 1
+
+    def _rec_generator(self, size, depth):
+        case = random.randint(0, 3)
+        depth -= 1
+        size -= size * depth / self._depth
+        if case == 0:
+            return self._list_creator(size, depth)
+        if case == 1:
+            return self._dict_creator(size, depth)
+        if case == 2:
+            return self._str_creator()
+        if case == 3:
+            return self._num_creator()
+
+    def _get_structure(self):
+        case = random.randint(0, 1)
+        res = []
+        if case:
+            res = {}
+            for i in range(self._size):
+                res[self._str_creator()] = self._rec_generator(self._size, self._depth)
+        else:
+            for i in range(self._size):
+                res.append(self._rec_generator(self._size, self._depth))
+        return res
