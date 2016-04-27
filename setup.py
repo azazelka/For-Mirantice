7829fbfc7b6c74b885dfd327ec0328821b269c1e
Anastasiya <atolochkova@mirantis.com>
2016-04-27 16:42:44 +0300

    Add tox

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yaml_parser',
+    version='1.3',
+    packages=find_packages(),
+    install_requires=[
+        "PyYAML"
+    ],
+)
